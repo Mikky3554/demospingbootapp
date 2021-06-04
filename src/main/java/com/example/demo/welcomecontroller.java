83d8dd4cb87e92d18eeb91b9342ee2bf4f0e50fa
Mikky3554 <teja3535@gmail.com>
2021-06-05 02:36:35 +0530

    Update welcomecontroller.java

--- src/main/java/com/example/demo/welcomecontroller.java
+++ src/main/java/com/example/demo/welcomecontroller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class welcomecontroller {
 
-	@GetMapping("/welcome")
+	@GetMapping("/")
 	public String welcome() {
 		return "First Jar Deployment";
 	}
