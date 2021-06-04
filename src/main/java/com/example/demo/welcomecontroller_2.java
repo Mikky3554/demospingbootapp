36bef4f89bedbdec7d21e096488bd3c239df9f39
Mikky3554 <teja3535@gmail.com>
2021-06-05 02:37:20 +0530

    Update welcomecontroller_2.java

--- src/main/java/com/example/demo/welcomecontroller_2.java
+++ src/main/java/com/example/demo/welcomecontroller_2.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class welcomecontroller_2 {
 
-	@GetMapping("/welcome2")
+	@GetMapping("/")
 	public String welcome() {
 		return "docker file Deployment";
 	}
